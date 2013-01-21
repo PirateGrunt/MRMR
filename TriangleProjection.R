ac1b5f6e97a1b58d86dc50c3e4669828f7a8ca29
BrianFannin <BFannin@RedwoodsGroup.com>
2013-01-20 22:26:20 -0500

    Momentarily removed summary and plot methods

--- TriangleProjection.R
+++ TriangleProjection.R
@@ -1,17 +1,29 @@
+
 setClass("TriangleProjection", 
-         representation(data = "dataframe"
-                        , name = "character"
-                        , 
-                        , LossPeriodInterval = "Interval"))
+         representation(ProjectionData = "dataframe"
+                        , ProjectionName = "character"
+                        , BaseTriangle = "Triangle"))
+
+setValidity("TriangleProjection"
+            , function(object){
+              if (is.Triangle(object@BaseTriangle)){
+                return (TRUE)
+              }else{
+                return ("Triangle projection must be based on a valid triangle.")
+              }
+              }
+            })
+
+
 
 # summary will have ability to write TEX output.
-setMethod("summary", "TriangleProjection",
-          function(object, ...) {
-            print(paste("Loss period type = ", object@LossPeriodType))
-            print(paste("Loss period interval = ", object@LossPeriodInterval))
-          })
-
-setMethod("plot", "TriangleProjection",
-          function(x, y, ...) {
-            
-          })
+# setMethod("summary", "TriangleProjection",
+#           function(object, ...) {
+#             print(paste("Loss period type = ", object@LossPeriodType))
+#             print(paste("Loss period interval = ", object@LossPeriodInterval))
+#           })
+# 
+# setMethod("plot", "TriangleProjection",
+#           function(x, y, ...) {
+#             
+#           })
