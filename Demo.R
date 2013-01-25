70b5110a28fed4bb1f5c9282514aee0886b6f973
BrianFannin <BFannin@RedwoodsGroup.com>
2013-01-24 22:00:40 -0500

    Minor changes to be consistent with TriangleModel object

--- Demo.R
+++ Demo.R
@@ -1,16 +1,28 @@
 # Demo script
+
+#=============================
+# Source the necessary code
 source("https://raw.github.com/PirateGrunt/MRMR/master/RegressionSupport.r")
 source("https://raw.github.com/PirateGrunt/MRMR/master/NAIC.R")
 source("https://raw.github.com/PirateGrunt/MRMR/master/ReservingVisualization.R")
 source("https://raw.github.com/PirateGrunt/MRMR/master/Triangle.R")
+source("https://raw.github.com/PirateGrunt/MRMR/master/TriangleModel.R")
+source("https://raw.github.com/PirateGrunt/MRMR/master/TriangleProjection.R")
 
+#=============================
+# Get some data from the big NAIC database 
+# and get a triangle we can project
 df = GetNAICData("wkcomp_pos.csv")
 bigCompany = as.character(df[which(df$CumulativePaid == max(df$CumulativePaid)),"GroupName"])
 
 df.BigCo = subset(df, GroupName == bigCompany)
 
 df.UpperTriangle = subset(df.BigCo, DevelopmentYear <=1997)
+df.LowerTriangle = subset(df.BigCo, DevelopmentYear > 1997)
 
+#=============================
+# Construct the triangle and display 
+# some basic properties
 tri = Triangle(TriangleData = df.UpperTriangle
                , TriangleName = bigCompany
                , LossPeriodType = "accident"
@@ -34,12 +46,32 @@ length(LatestDiagonal(tri)[,1])
 plt = ShowTriangle(tri@TriangleData, bigCompany, Cumulative=FALSE)
 #Note the apparent calendar year impact in 1996. This is invisible in the cumulative display.
 
+setName(tri) = "AnotherName"
+tri@TriangleName
+setName(tri) = bigCompany
+tri@TriangleName
+tri@TriangleName = "Another name"
+
+#===========================
+# Now let's fit a couple of models
+
+chainLadder = TriangleModel("CumulPaid"
+                            , BaseTriangle = tri
+                            , ResponseName = "CumulativePaid"
+                            , PredictorName = "DirectEP"
+                            , CategoryName = "DevelopmentLag"
+                            , MinimumCategoryFrequency = 1
+                            , delta = 0)
+summary(chainLadder@LinearFit)
+
+
+
+df.LowerTriangle$Cat = as.factor(df.LowerTriangle$DevelopmentLag)
+df.LowerTriangle$prediction = predict.lm(fit, newdata = df.LowerTriangle)
+df.LowerTriangle$Error = with(df.LowerTriangle, prediction - CumulativePaid)
+
 # To calibrate a multiplicative chain ladder, we must omit the first development lag.
 df.UpperTriangle = subset(df.UpperTriangle, DevelopmentLag >1)
-
-fit = with( df.UpperTriangle, FitModel(IncrementalPaid, PriorCumulativePaid, DevelopmentLag, 1, 0))
-
-devFactors = CreateDevelopmentArray(fit, 1, 10)
 
 # To project a multiplicative chain ladder, we may only use the latest diagonal.
 df.Latest = subset(df.BigCo, DevelopmentYear == 1997)
