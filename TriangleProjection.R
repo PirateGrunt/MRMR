c1dea8fd52d61d4fca34b521217b57c692dad65d
BrianFannin <BFannin@RedwoodsGroup.com>
2013-01-24 22:01:39 -0500

    Added TriangleProjection constructor
    This is still under construction

--- TriangleProjection.R
+++ TriangleProjection.R
@@ -1,21 +1,48 @@
 
 setClass("TriangleProjection", 
-         representation(ProjectionData = "dataframe"
-                        , ProjectionName = "character"
-                        , BaseTriangle = "Triangle"))
+         representation(ProjectionName = "character"
+                        , TriangleModel = "TriangleModel"
+                        , AsOfDate = "POSIXct"))
 
 setValidity("TriangleProjection"
             , function(object){
-              if (is.Triangle(object@BaseTriangle)){
                 return (TRUE)
-              }else{
-                return ("Triangle projection must be based on a valid triangle.")
-              }
               }
             })
 
-
-
+TriangleProjection = function(ProjectionName
+                              , Model
+                              , AsOfDate)
+{
+  # count the number of periods between
+  Triangle = Model@BaseTriangle
+  df = Triangle@TriangleData
+  
+  latestDate = max(df$EvaluationDate)
+  span = as.duration(AsOfDate - latestDate)
+  
+  # still not sure how to calculate the number of intervals
+  latestDate + tri@DevelopmentInterval
+  
+  mojo = as.period(new_interval(latestDate, AsOfDate))
+  ProjectionIntervals = mojo / tri@DevelopmentInterval
+  # TODO: add a check for a remainder
+  
+  DevIntervals = (1:ProjectionIntervals) * tri@DevelopmentInterval
+  DevIntervals
+  EvalDates = latestDate + DevIntervals
+  EvalDates
+  
+  df = 
+  
+  proj = new("TriangleProjection"
+             , ProjectionName = ProjectionName
+             , TriangleModel = Model
+             , AsOfDate = AsOfDate)
+  
+  return (proj)
+}
+#==============
 # summary will have ability to write TEX output.
 # setMethod("summary", "TriangleProjection",
 #           function(object, ...) {
