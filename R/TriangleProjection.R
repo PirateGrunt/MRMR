b729654d2e4a8148f8d838e99b1947d062a2c94f
BrianFannin <BFannin@RedwoodsGroup.com>
2013-07-20 14:09:09 -0400

    TriangleProjection
    various

--- R/TriangleProjection.R
+++ R/TriangleProjection.R
@@ -12,36 +12,60 @@ setValidity("TriangleProjection"
               }
             )
 
-GetProjectionDates = function(LatestEvalDate, ProjectionDate, DevelopmentInterval){
+GetProjectionDates = function(rowNum, df, ProjectionDate, DevelopmentInterval, ResponseName){
+  
+  # check that the df holds a column called "EvaluationDate"
+  # check that the DevelopmentInterval is positive
   
   require(lubridate)
-  
-  ProjectionInterval = as.period(new_interval(LatestEvalDate, ProjectionDate))
+  df = df[rowNum,]
+  ProjectionInterval = new_interval(df$EvaluationDate + days(1), ProjectionDate)
   # TODO: add a check for a remainder
   ProjectionIntervals = ProjectionInterval / DevelopmentInterval
-  DevIntervals = (1:ProjectionIntervals) * DevelopmentInterval
-  EvalDates = LatestEvalDate + DevIntervals
+  print(ProjectionIntervals)
   
-  return (as.data.frame(EvalDates))
+  aList = replicate(ProjectionIntervals, df, simplify=FALSE)
+  df = do.call("rbind", aList)
+  
+  DevIntervals = (1:ProjectionIntervals) * DevelopmentInterval
+  
+  df$EvaluationDate = df$EvaluationDate + DevIntervals
+  df$DevelopmentLag = as.period(new_interval(df$OriginPeriodStart, df$EvaluationDate + days(1)))
+  df$DevelopmentMultiplier = df$DevelopmentLag / DevelopmentInterval
+  df[,ResponseName] = 0
+  
+  return (df)
+}
+
+GetProjectedValues = function(df, fit, PredictorName, CategoryName)
+{
+  if (CategoryName == "none"){
+    newX = df[, colnames(df) %in% PredictorName]
+    } else {
+    newX = df[, colnames(df) %in% c(PredictorName, CategoryName)]
+  }
+  print(summary(newX))
+  mojo = predict(fit, newX)
+  return (mojo)
 }
 
 TriangleProjection = function(ProjectionName
                               , Model
-                              , AsOfDate)
+                              , ProjectTo = "ProjectDate"
+                              , ProjectDate
+                              , ProjectLag = NULL)
 {
-  Model = chainLadder
-  Triangle = Model@BaseTriangle
-  df = Triangle@TriangleData
+  tri = Model@Triangle
   
-  df.latest = LatestDiagonal(Triangle)
-  latestDate = df.latest$EvaluationDate
+  df.latest = LatestDiagonal(tri)
   
-  mojo = as.data.frame(GetProjectionDates(latestDate[1], AsOfDate, Triangle@DevelopmentInterval))
-  
-  mojo = lapply(latestDate, GetProjectionDates, AsOfDate, Triangle@DevelopmentInterval)
+  rowNums = 1:nrow(df.latest)
+  mojo = lapply(rowNums, GetProjectionDates
+                        , df.latest
+                        , ProjectToDate
+                        , tri@DevelopmentInterval
+                        , Model@ResponseName)
   dfResults = do.call("rbind", mojo)
-  
-  df = 
   
   proj = new("TriangleProjection"
              , ProjectionName = ProjectionName
@@ -50,16 +74,3 @@ TriangleProjection = function(ProjectionName
   
   return (proj)
 }
-
-#==============
-# summary will have ability to write TEX output.
-# setMethod("summary", "TriangleProjection",
-#           function(object, ...) {
-#             print(paste("Loss period type = ", object@LossPeriodType))
-#             print(paste("Loss period interval = ", object@LossPeriodInterval))
-#           })
-# 
-# setMethod("plot", "TriangleProjection",
-#           function(x, y, ...) {
-#             
-#           })
