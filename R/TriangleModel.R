244688faa3876668c46953a35484a1e04c929792
BrianFannin <BFannin@RedwoodsGroup.com>
2013-07-21 13:57:38 -0400

    TriangleModel
    Changed construction of formula

--- R/TriangleModel.R
+++ R/TriangleModel.R
@@ -1,82 +1,57 @@
-#' @include Triangle.R
-NULL
-
-setClass("TriangleModel", 
-         representation(TriangleModelName = "character"
-                        , BaseTriangle = "Triangle"
-                        , Response = "character"
-                        , Predictor = "character"
-                        , Category = "character"
-                        , MinimumCategoryFrequency = "numeric"
-                        , delta = "numeric"
-                        , LinearFit = "lm"))
-
-# setValidity("TriangleModel"
-#             , function(object){
-#               if (is.Triangle(object@BaseTriangle)){
-#                 return (TRUE)
-#               }else{
-#                 return ("Triangle projection must be based on a valid triangle.")
-#               }
-#             })
-
-#============================
-# Constructor
-TriangleModel = function(ModelName
-                         , BaseTriangle
-                         , ResponseName
-                         , PredictorName
-                         , CategoryName = "none"
-                         , MinimumCategoryFrequency = 1
-                         , delta = 0)
-{  
-  # Check that the various columns exist. We'll shorthand the Triangle dataframe.
-  df = BaseTriangle@TriangleData
-  
-  if (!ColumnExists(df, ResponseName))
-  {
-    stop ("The specified column for the response variable does not exist. Unable to create the triangle model.")
-  }
-  
-  if (!ColumnExists(df, PredictorName))
-  {
-    stop ("The specified column for the predictor variable does not exist. Unable to create the triangle model.")
-  }
-  
-  if (!ColumnExists(df, CategoryName) && CategoryName != "none")
-  {
-    stop ("The specified column for the categorical variable does not exist. Unable to create the triangle model.")
-  }
-  
-  # Now calibrate the model
-  theFormula = as.formula(paste0(ResponseName, " ~ ", PredictorName, ":", CategoryName, " + 0"))
-  if (tolower(CategoryName) == "none"){
-    theFormula = as.formula(paste0(ResponseName, " ~ ", PredictorName, " + 0"))
-  } 
-  
-  fit = lm(theFormula, data = df)
-  
-  triModel = new("TriangleModel"
-                 , TriangleModelName = ModelName
-                 , BaseTriangle = BaseTriangle
-                 , Response = ResponseName 
-                 , Predictor = PredictorName
-                 , Category = CategoryName
-                 , MinimumCategoryFrequency = MinimumCategoryFrequency
-                 , delta = delta
-                 , LinearFit = fit)
-  
-  return (triModel)
+is.TriangleModel = function(object)
+{
+  is(object, "TriangleModel")
 }
 
-# setGeneric("CalibrateTriangleModel", function(x, ...){
-#   standardGeneric("CalibrateTriangleModel")
-# })
-# 
-# setMethod("CalibrateTriangleModel", "Triangle", function(x, Response, Predictor, Category, delta, MinimumCategoryFrequency){
-#   
-#   df = BaseTriangle@TriangleData
-#   
-#   
-#   return(fit)
-# })
+checkTriangleModel = function(object)
+{
+  errors = character()
+
+  if (length(errors) == 0) TRUE else errors
+}
+
+#' TriangleModel class
+#' 
+#' Triangle is an S4 class used to store a model fit to a Triangle object.
+#' 
+#' @name TriangleModel-class
+#' @rdname TriangleModel-class
+#' @exportClass TriangleModel
+#' 
+setClass("TriangleModel"
+         , representation(Triangle = "Triangle"
+                          , Response = "character"
+                          , Predictor = "character"
+                          , Group = "character"
+                          , Fit = "lm")
+         #          , sealed = TRUE
+         , validity = #some function
+)
+
+newTriangleModel = function(Triangle
+                          , Response
+                          , Predictor
+                          , Group
+                          , Intercept = FALSE
+                          , Weights = NULL)
+{
+  df = Triangle@TriangleData
+  
+  strFormula = paste0(Response, " ~ ", Predictor, ":as.factor(", Group, ")")
+  
+  if (Intercept){
+    strFormula = paste0(strFormula, " + 1")
+  } else {
+    strFormula = paste0(strFormula, " + 0")
+  }
+  
+  theFormula = as.formula(strFormula)
+  
+  Fit = lm(theFormula, data = df)
+  fit = new("TriangleModel"
+            , Response = Response
+            , Predictor = Predictor
+            , Group = Group
+            , Fit = Fit)
+  fit
+}
