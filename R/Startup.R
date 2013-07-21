198c718c37245d52c33c40a6721066831e94b407
BrianFannin <BFannin@RedwoodsGroup.com>
2013-07-21 13:57:09 -0400

    Startup - load data files
    For some reason, lapply doesn't seem to work with GitHub

--- R/Startup.R
+++ R/Startup.R
@@ -14,15 +14,50 @@ sourceFiles = c("TriangleOriginPeriod.R"
                 , "TriangleOriginPeriod.R"
                 , "TriangleAdjustMeasures.R"
                 , "Triangle.R"
-                , "TriangleMeta.R")
+                , "TriangleMeta.R"
+                , "PlotTriangle.R")
 
 sourceDirectory = "https://raw.github.com/PirateGrunt/MRMR/master/R/"
 
 invisible(lapply(paste0(sourceDirectory, sourceFiles), source))
 
-dataDirectory = "https://raw.github.com/PirateGrunt/MRMR/master/Data/"
+library(RCurl)
+dataURL = "https://github.com/PirateGrunt/MRMR/blob/master/data/"
 dataFiles = c("NAIC.rda", "Multiline.rda", "Friedland.rda")
-invisible(lapply(paste0(dataDirectory, dataFiles), load))
+dataURLStem = "?raw=true"
 
+LoadDataFromGitHub = function(url)
+{
+  sit = getURLContent(url, binary=TRUE, followlocation = TRUE, ssl.verifypeer = FALSE)
+  con = gzcon(rawConnection(sit, 'rb'))
+  eval(load(con), envir=globalenv())
+  1
+#  close(con)
+}
 
-rm(supportLibraries, sourceFiles, sourceDirectory, dataFiles, dataDirectory)
+# LoadDataFromGitHub(mojo[3])
+# 
+# lapply(paste0(dataURL, dataFiles, dataURLStem), LoadDataFromGitHub)
+# 
+mojo = paste0(dataURL, dataFiles, dataURLStem)
+
+url = mojo[1]
+sit = getURLContent(url, binary=TRUE, followlocation = TRUE, ssl.verifypeer = FALSE)
+con = gzcon(rawConnection(sit, 'rb'))
+load(con)
+
+url = mojo[2]
+sit = getURLContent(url, binary=TRUE, followlocation = TRUE, ssl.verifypeer = FALSE)
+con = gzcon(rawConnection(sit, 'rb'))
+load(con)
+
+url = mojo[3]
+sit = getURLContent(url, binary=TRUE, followlocation = TRUE, ssl.verifypeer = FALSE)
+con = gzcon(rawConnection(sit, 'rb'))
+load(con)
+
+close(con)
+
+rm(mojo, con, url, sit)
+
+rm(supportLibraries, sourceFiles, sourceDirectory, dataFiles, dataURL, dataURLStem, LoadDataFromGitHub)
