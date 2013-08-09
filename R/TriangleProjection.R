0fce73d221ab4a2ca5f6078cec654e943d56f5a3
BrianFannin <BFannin@RedwoodsGroup.com>
2013-08-09 10:18:03 -0400

    Moved GetProjectionDates

--- R/TriangleProjection.R
+++ R/TriangleProjection.R
@@ -1,76 +1,43 @@
 #' @include Triangle.R
 NULL
 
-setClass("TriangleProjection", 
-         representation(ProjectionName = "character"
-                        , TriangleModel = "TriangleModel"
-                        , AsOfDate = "POSIXct"))
+setClass("TriangleProjection"
+         , representation(TriangleModel = "TriangleModel"
+                          , ProjectToDev = "logical"
+                          , MaxDev = "numeric"
+                          , AsOfDate = "POSIXct"
+                          , ProjectionData = "data.frame"))
 
-setValidity("TriangleProjection"
-            , function(object){
-                return (TRUE)
-              }
-            )
-
-GetProjectionDates = function(rowNum, df, ProjectionDate, DevelopmentInterval, ResponseName){
-  
-  # check that the df holds a column called "EvaluationDate"
-  # check that the DevelopmentInterval is positive
-  
-  require(lubridate)
-  df = df[rowNum,]
-  ProjectionInterval = new_interval(df$EvaluationDate + days(1), ProjectionDate)
-  # TODO: add a check for a remainder
-  ProjectionIntervals = ProjectionInterval / DevelopmentInterval
-  print(ProjectionIntervals)
-  
-  aList = replicate(ProjectionIntervals, df, simplify=FALSE)
-  df = do.call("rbind", aList)
-  
-  DevIntervals = (1:ProjectionIntervals) * DevelopmentInterval
-  
-  df$EvaluationDate = df$EvaluationDate + DevIntervals
-  df$DevelopmentLag = as.period(new_interval(df$OriginPeriodStart, df$EvaluationDate + days(1)))
-  df$DevelopmentMultiplier = df$DevelopmentLag / DevelopmentInterval
-  df[,ResponseName] = 0
-  
-  return (df)
-}
-
-GetProjectedValues = function(df, fit, PredictorName, CategoryName)
+TriangleProjection = function(objTriangleModel
+                              , ProjectToDev = TRUE
+                              , MaxDev = 10
+                              , AsOfDate = mdy("12/31/2010"))
 {
-  if (CategoryName == "none"){
-    newX = df[, colnames(df) %in% PredictorName]
-    } else {
-    newX = df[, colnames(df) %in% c(PredictorName, CategoryName)]
+  dfLatest = LatestDiagonal(objTriangleModel@ModelData)
+  
+  lOriginYear = dlply(dfLatest, "OriginPeriodStart")
+  
+  if (ProjectToDev){
+    dfProjection = ProjectToDev(objTriangleModel, lOriginYear, MaxDev)
+  } else {
+    dfProjection = ProjectToDate(objTriangleModel, lOriginYear, AsOfDate)
   }
-  print(summary(newX))
-  mojo = predict(fit, newX)
-  return (mojo)
-}
-
-TriangleProjection = function(ProjectionName
-                              , Model
-                              , ProjectTo = "ProjectDate"
-                              , ProjectDate
-                              , ProjectLag = NULL)
-{
-  tri = Model@Triangle
   
-  df.latest = LatestDiagonal(tri)
+  if (objTriangleModel@FitCategory == "DevInteger"){
+    dfProjection$FitCategory = TailFunction(dfProjection$DevInteger, objTriangleModel@Tail)
+    dfProjection$FitCategory = factor(dfProjection$FitCategory)
+  }
   
-  rowNums = 1:nrow(df.latest)
-  mojo = lapply(rowNums, GetProjectionDates
-                        , df.latest
-                        , ProjectToDate
-                        , tri@DevelopmentInterval
-                        , Model@ResponseName)
-  dfResults = do.call("rbind", mojo)
+  dfProjection[objTriangleModel@Response] = ProjectValues(objTriangleModel, dfProjection)
+  
+  row.names(dfProjection) = NULL
   
   proj = new("TriangleProjection"
-             , ProjectionName = ProjectionName
-             , TriangleModel = Model
-             , AsOfDate = AsOfDate)
+             , TriangleModel = objTriangleModel
+             , ProjectToDev = ProjectToDev
+             , MaxDev = MaxDev
+             , AsOfDate = AsOfDate
+             , ProjectionData = dfProjection)
   
-  return (proj)
+  proj
 }
