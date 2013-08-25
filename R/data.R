840b6c2ee4eb830f48d0fb48f4996b528d271018
BrianFannin <BFannin@RedwoodsGroup.com>
2013-08-25 14:22:36 -0400

    data.R remove reference to NAIC

--- R/data.R
+++ R/data.R
@@ -1,14 +1,3 @@
-#' NAIC workers comp data
-#' 
-#' This data set contains workers comp development information obtained from public filings. The data was gathered and prepared
-#' by Glenn Meyers and Peng Shi.
-#' 
-#' @name NAIC
-#' @docType data
-#' @references \url{http://www.casact.org/research/index.cfm?fa=loss_reserves_data}
-#' @keywords data NAIC
-NULL
-
 #' Friedland data
 #' 
 #' This is a single triangle taken from blah by Jacqueline Friedland, page 65.
@@ -21,7 +10,16 @@ NULL
 #' 
 #' This data set is taken from the NAIC data prepared by Glenn Meyers and Peng Shi
 #' 
-#' @name multiline
+#' @name Multiline
 #' @docType data
 #' @references \url{http://www.casact.org/research/index.cfm?fa=loss_reserves_data}
+#' @keywords data NAIC
+NULL
+
+#' Mack data
+#' 
+#' RAA data from Mack's paper
+#' 
+#' @name Mack
+#' @docType data
 NULL
