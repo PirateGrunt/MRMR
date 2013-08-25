8131e7c83ff0c51c00df48fcb320f994cf88c88b
BrianFannin <BFannin@RedwoodsGroup.com>
2013-08-25 14:27:29 -0400

    roxygen

--- R/TriangleAdjustMeasures.R
+++ R/TriangleAdjustMeasures.R
@@ -2,27 +2,32 @@
 #' 
 #' @export CreateIncrementals
 #' 
-#' @param OriginPeriod Vector of intervals
-#' @param Group A vector of groups
-#' @param Variable A list of 
-#' @param StartDay If OriginStart and OriginEnd are supplied, this argument is ignored.
-#' @param StartMonth If OriginStart and OriginEnd are supplied, this argument is ignored.
+#' @param dfTriangleData A data frame of triangle variables
+#' @param measureCols A character vector which holds column names identifying stochastic measures
+#' @param Groups A character vector which holds column names identifying groups
 #' 
-#' @return A vector of intervals
+#' @return A data frame of measures which includes incrementals
 #' 
 #' @seealso \code{\link{CreateCumulative}}, \code{\link{CreatePriors}}
 #' 
 CreateIncrementals = function(dfTriangleData, measureCols, Groups)
 {
   require(plyr)
+  
+  incrColNames = gsub("Cumulative", "Incremental", measureCols)
+  
   lOriginYear = dlply(dfTriangleData, c(Groups, "OriginPeriodStart"))
   
   lOriginYear = lapply(lOriginYear, function(x) {
     x = x[order(x$OriginPeriodStart, x$EvaluationDate),]
-    theMeasures = x[, measureCols]
-    incrementals = apply(theMeasures, 2, diff)
-    incrementals = rbind(theMeasures[1,], incrementals)
-    colnames(incrementals) = gsub("Cumulative", "Incremental", measureCols)
+    theMeasures = x[measureCols]
+    if (nrow(theMeasures) == 1) {
+      incrementals = theMeasures
+    } else {
+      incrementals = as.data.frame(apply(theMeasures, 2, diff))
+      incrementals = rbind(theMeasures[1,], incrementals)
+    }
+    names(incrementals) = incrColNames
     x = cbind(x, incrementals)
   })
   dfMeasures = do.call("rbind", lOriginYear)
@@ -35,10 +40,10 @@ CreateIncrementals = function(dfTriangleData, measureCols, Groups)
 #' Create cumulative
 #' 
 #' @export CreateCumulative
-#' @param OriginPeriod Vector of intervals
-#' @param Group A vector of groups
-#' @param Variable A list of 
-#' @return Adjusted triangle
+#' @param dfTriangleData A data frame of triangle variables
+#' @param measureCols A character vector which holds column names identifying stochastic measures
+#' @param Groups A character vector which holds column names identifying groups
+#' @return A data frame of measures with cumulatives included
 #' @seealso \code{\link{CreateIncrementals}}, \code{\link{CreatePriors}}
 #' 
 CreateCumulative = function(dfTriangleData, measureCols, Groups)
@@ -70,11 +75,12 @@ CreateCumulative = function(dfTriangleData, measureCols, Groups)
 #' Create priors
 #' 
 #' @export CreatePriors
-#' @param OriginPeriod Vector of intervals
-#' @param Group A vector of groups
-#' @param Variable A list of 
-#' @return Adjusted triangle
-#' @seealso \code{\link{CreateIncremental}}, \code{\link{CreateCumulative}}
+#' @param dfTriangleData A data frame of triangle variables
+#' @param measureCols A character vector which holds column names identifying stochastic measures
+#' @param Groups A character vector which holds column names identifying groups
+#' 
+#' @return A data frame of measures which includes prior values
+#' @seealso \code{\link{CreateIncrementals}}, \code{\link{CreateCumulative}}
 #' 
 CreatePriors = function(dfTriangleData, measureCols, Groups)
 {
@@ -111,6 +117,8 @@ CreatePriors = function(dfTriangleData, measureCols, Groups)
 #' 
 #' @export FormMeasureNames
 #' 
+#' @param Measures A character vector of stochastic measure names
+#' @param Cumulative Boolean indicating whether the measure names are cumulative or incremental
 FormMeasureNames = function(Measures, Cumulative = TRUE)
 {
   if (Cumulative){
@@ -135,6 +143,9 @@ CleanMeasureNames = function(MeasureNames){
 #' 
 #' @export GetStochasticColumnNames
 #' 
+#' @param MeasureNames A character vector of base measure names
+#' 
+#' @return A character vector of measure names augmented with the words Incremental, Cumulative and Prior
 GetStochasticColumnNames = function(MeasureNames){
   baseNames = CleanMeasureNames(MeasureNames)
   incrNames = paste0("Incremental", baseNames)
